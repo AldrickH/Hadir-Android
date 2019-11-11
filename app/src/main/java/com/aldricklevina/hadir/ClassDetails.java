929fb9e6bf49bac51de1230d7bbb53bd45cd84f2
Aldrick Handinata <aldrickhandinata1@gmail.com>
2019-11-11 23:47:30 +0700

    Code Refactor and add login page

--- app/src/main/java/com/aldricklevina/hadir/ClassDetails.java
+++ app/src/main/java/com/aldricklevina/hadir/ClassDetails.java
@@ -1,34 +1,73 @@
 package com.aldricklevina.hadir;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.ListFragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class ClassDetails extends AppCompatActivity {
 
-    private ArrayList<String> arrayMurid;
-    private ListView listView;
+    private ArrayList<String> listStudent;
+    private ListView listViewStudent;
+    private TextView txtSchedule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_details);
 
-//        listView = (ListView) findViewById(R.id.listView);
-//        arrayMurid = new ArrayList<String>();
-//
-//        arrayMurid.add(0, "Aldrick Handinata");
-//        arrayMurid.add(1, "Levina Khomulia");
-//        arrayMurid.add(2, "Secretary Han");
+        listViewStudent = findViewById(R.id.listViewStudentClass);
+        txtSchedule = findViewById(R.id.txtSchedule);
+        listStudent = new ArrayList<String>();
 
-//        ListAdapter listAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, arrayMurid);
-//        setListAdapter(listAdapter);
+        listStudent.add(0, "Aldrick Handinata");
+        listStudent.add(1, "Levina Khomulia");
+        listStudent.add(2, "Secretary Han");
+
+        ListAdapter listAdapter = new ArrayAdapter<String>(ClassDetails.this, android.R.layout.simple_list_item_multiple_choice, listStudent) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+
+                // Set the border of View (ListView Item)
+                view.setBackground(getContext().getDrawable(R.drawable.rounded_corner));
+
+                // panggil layout list_mhs
+//                listView.Layout
+//                listView.setLayoutMode();
+//                LayoutInflater inflater = (LayoutInflater) .getLayoutInflater();
+//                View v = inflater.inflate(R.layout.list_mhs, null);
+//
+//                TextView namaMhs;
+//                namaMhs = v.findViewById(R.id.txtNamaMhs);
+//                namaMhs.setText(listView[position]);
+
+                // Return the view
+                return view;
+            }
+        };
+
+        listViewStudent.setAdapter(listAdapter);
+
+//        listStudent.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, arrayMurid));
+        listViewStudent.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+
+        txtSchedule.setText(currentDate);
+
+//        listStudent.setBackgroundResource(R.drawable.rounded_corner);
 
     }
 }
--- app/src/main/java/com/aldricklevina/hadir/HomepageViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.aldricklevina.hadir;
-
-import androidx.lifecycle.ViewModel;
-
-public class HomepageViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
